7630f8f0aa4b9983a5c23be6f848c0f7f952e5d0
Ubuntu <ubuntu@ip-172-31-9-43.eu-west-1.compute.internal>
2023-07-20 20:27:59 +0000

    extend test

--- lifetime_analysis/test/test.cc
+++ lifetime_analysis/test/test.cc
@@ -10,17 +10,61 @@ using namespace lifetimes;
 int main()
 {
     LifetimeDemo x;
+
     std::string test = R"(
+    #include <cstdio>
+    #include <iostream>
     int* target(int* a, int* b, unsigned x) {
       int* v[2] = {a, b};
+      std::cout << "test";
       return v[x & 1];
     }
+    )";
 
-    void nontarget(int** array, int* p, int* q) {
+    std::string test1 = R"(
+    void target(int** array, int* p, int* q) {
       array[0] = p;
       array[1] = q;
     }
     )";
-    std::cout << x.GetLifetimes(test) << "\n";
+
+    std::string test2 = R"(
+    void DemoServiceImpl::hello(const std::string& hi, std::string* reply, rrr::DeferredReply* defer) {
+        *reply += std::string("Re: ") + hi;
+        defer->reply();
+    }
+
+    void DemoServiceImpl::sum(const rrr::i32& a, const rrr::i32& b, const rrr::i32& c, rrr::i32* result, rrr::DeferredReply* defer) {
+        *result = a + b + c;
+        defer->reply();
+    }
+    )";
+
+    std::string test3 = R"(
+      int* f(int* a) {
+        return a;
+      }
+
+      int* target(int* a) {
+        return f(a);
+      }
+    )";
+
+    std::string test4 = R"(
+      int* target(int* a) {
+        return a;
+      }
+
+      int* f(int* x) {
+        int y = 2;
+        return &y;
+      }
+    )";
+
+    std::cout << x.GetLifetimes(test) << "\t#0\n";
+    std::cout << x.GetLifetimes(test1) << "\t#1\n";
+    std::cout << x.GetLifetimes(test2) << "\t#2\n";
+    std::cout << x.GetLifetimes(test3) << "\t#3\n";
+    std::cout << x.GetLifetimes(test4) << "\t#4\n";
     return 0;
 }
